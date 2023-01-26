e37896b68b55d900154d173e555f09cf1cc55389
mathieu <mathecool22@gmail.com>
2023-01-26 11:29:19 -0500

    petit menage dans la facon de structurer le code

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import Header from "../components/header";
+
 const About = () => {
-	return <div>Enter</div>;
+	return (
+		<>
+			<div>Enter</div>
+		</>
+	);
 };
 
 export default About;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
 	return (
 		<>
-			<Header />
 			<main className={styles.main}>
 				<div className={styles.description}>
 					<p>
